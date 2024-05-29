11f366175d97c1831a86486b54d9ddbbe0d7929f
Ethan Tidmore <ethantidmore06@gmail.com>
2024-05-29 16:50:08 -0500

    alphabetilize

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ CC=cc
 CFLAGS="-Wall -Wextra -O2"
 BIN="al"
 
-usage() {
-	echo "Usage: $0 [build clean]" 
-}
-
 build() {
 	$CC $CFLAGS al.c -o $BIN
 }
 
 clean() {
 	rm $BIN
+}
+
+usage() {
+	echo "Usage: $0 [build clean]" 
 }
 
 case "$1" in
